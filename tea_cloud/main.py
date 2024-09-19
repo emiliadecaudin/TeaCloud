257156a0c07faf174fc3e3c4e7e9dc0d0a258a0f
Émilia Decaudin <emilia.decaudin@gmail.com>
2024-09-19 15:22:41 -0400

    Use text directly.

--- tea_cloud/main.py
+++ tea_cloud/main.py
@@ -1,4 +1,3 @@
-from collections import Counter
 from datetime import datetime, timedelta
 from logging import getLogger
 from os import getenv
@@ -7,16 +6,12 @@ from typing import Final
 import discord.client
 import discord.flags
 from dotenv import load_dotenv
-from wordcloud import STOPWORDS, WordCloud
+from wordcloud import WordCloud
 
 logger = getLogger("discord")
 load_dotenv()
 BOT_TOKEN: Final = getenv("BOT_TOKEN", "")
 TWENTY_FOUR_HOURS_AGO: Final = datetime.today() - timedelta(days=1)
-
-
-def get_words(text: str) -> list[str]:
-    return list(filter(lambda x: len(x) > 0, text.replace("\n", " ").split(" ")))
 
 
 def get_text_channels(client: discord.client.Client) -> set[discord.TextChannel]:
@@ -35,20 +30,17 @@ client = discord.client.Client(intents=intents)
 
 @client.event
 async def on_ready() -> None:
-    logger.info("Collecting words...")
-    words = Counter(
+    logger.info("Collecting messages...")
+    text = " ".join(
         [
-            word
+            message.content
             for channel in get_text_channels(client)
             async for message in channel.history(after=TWENTY_FOUR_HOURS_AGO)
-            for word in get_words(message.content)
         ]
     )
-    for stopword in STOPWORDS:
-        del words[stopword]
 
     logger.info("Generating word cloud...")
-    WordCloud().generate_from_frequencies(words).to_file("./output.png")
+    WordCloud().generate(text).to_file("./output.png")
     logger.info("Word cloud generated...")
 
 
