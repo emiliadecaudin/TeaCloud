be66b25ad35820daa60fab3209d2adc2f6e8cd34
Émilia Decaudin <emilia.decaudin@gmail.com>
2024-09-19 22:13:39 -0400

    Add doug.

--- tea_cloud/main.py
+++ tea_cloud/main.py
@@ -4,9 +4,12 @@ from os import getenv
 from typing import Final
 
 import discord.client
+import discord.file
 import discord.flags
+import numpy
 from dotenv import load_dotenv
-from wordcloud import WordCloud
+from PIL import Image
+from wordcloud import ImageColorGenerator, WordCloud
 
 logger = getLogger("discord")
 load_dotenv()
@@ -31,17 +34,29 @@ client = discord.client.Client(intents=intents)
 @client.event
 async def on_ready() -> None:
     logger.info("Collecting messages...")
+
+    channels = get_text_channels(client)
+
     text = " ".join(
         [
             message.content
-            for channel in get_text_channels(client)
+            for channel in channels
             async for message in channel.history(after=TWENTY_FOUR_HOURS_AGO)
         ]
     )
 
     logger.info("Generating word cloud...")
-    WordCloud().generate(text).to_file("./output.png")
+    doug_mask = numpy.array(Image.open("doug_mask.png"))
+    WordCloud(
+        background_color="white",
+        mask=doug_mask,
+        color_func=ImageColorGenerator(doug_mask),
+    ).generate(text).to_file("output.png")
     logger.info("Word cloud generated...")
+
+    for channel in channels:
+        if channel.name == "tech":
+            await channel.send(file=discord.file.File("./output.png"))
 
 
 client.run(BOT_TOKEN)
